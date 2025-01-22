60f63ceedbfc31ed8d60e92233d6520ae598b15c
Priti Fuse <E010846@cigniti.com>
2025-01-22 22:20:40 +0530

    22jan

--- 22Jan/oop_1.py
+++ 22Jan/oop_1.py
@@ -1,4 +1,17 @@
 #oop : object oreintation programming
-
-
+#inheretance
+#encapsulation : hiding data
+#polymorfism
+#abstraction
 print("oop")
+class Employee:
+    def __init__(self,fname,lname,sal):
+        self.fname= fname
+        self.lname=lname
+        self.sal=sal
+
+harry=Employee("harry_name","das",10000)
+
+print(harry.sal)
+print(harry.fname)
+print(harry.lname)
