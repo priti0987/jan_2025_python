7bb2aba85078ff60151f3b0032d489e11184d86d
Priti Fuse <E010846@cigniti.com>
2025-01-23 21:47:47 +0530

    23jan

---
+++ 22Jan/aMethod.py
@@ -0,0 +1,27 @@
+class Employee:
+    number_of_emp = 0
+    increment = 10
+
+    def __init__(self, fname, lname, sal):
+        self.fname = fname
+        self.lname = lname
+        self.sal = sal
+        Employee.number_of_emp+=1
+
+    def increase(self):
+        self.sal = self.sal * Employee.increment
+
+    @classmethod
+    def change_increment(cls,amt):
+        cls.increment=amt
+
+    @classmethod
+    def from_str(cls,emp_string):
+        fname,lname,sal=emp_string.split("-")
+        return cls(fname,lname,sal)
+
+
+
+lvish=Employee.from_str("lovish-jacksn-30000")
+print(lvish.fname)
+print(lvish.sal)
