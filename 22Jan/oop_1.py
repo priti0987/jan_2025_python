f75f7f679109502cbf956d201755f913ee9888dd
Priti Fuse <E010846@cigniti.com>
2025-01-23 18:11:42 +0530

    23jan

--- 22Jan/oop_1.py
+++ 22Jan/oop_1.py
@@ -3,15 +3,21 @@
 #encapsulation : hiding data
 #polymorfism
 #abstraction
-print("oop")
 class Employee:
+    increment = 1.5
     def __init__(self,fname,lname,sal):
         self.fname= fname
         self.lname=lname
         self.sal=sal
+
+    def increase(self):
+        self.sal = self.sal* Employee.increment
 
 harry=Employee("harry_name","das",10000)
 
 print(harry.sal)
 print(harry.fname)
 print(harry.lname)
+print(harry.increment)
+harry.increase()
+print(harry.sal)
