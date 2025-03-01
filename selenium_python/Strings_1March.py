43d594e297bdc5271a6b71e84fe4232a9c6cee97
Priti Fuse <E010846@cigniti.com>
2025-03-01 09:21:36 +0530

    1March2025

--- selenium_python/Strings_1March.py
+++ selenium_python/Strings_1March.py
@@ -13,3 +13,19 @@ this is 3rd month of 2025 year'''
 # print("lenght of string1 ==> ",len(str1))
 # print("lower case of string 1==> ", str1.lower())
 # print("CHeck if string 1 is lower case==>",str1.islower())
+#
+# print("upper case of string 1==> ", str1.upper())
+# print("CHeck if string 1 is lower case==>",str1[1].isupper())
+
+# print("concat operation == >" , str1+str2)
+
+#find method will search for charater in string , if charater is not there it will return -1
+# print(str1.find("a"))
+# #find method will return index of 1st occrance of charator , if multiple charactes are there
+# print(str1.find("i"))
+
+# print(str1.replace("i","I"))
+# print("#".join(str1))
+# print("P".join(str1))
+
+
